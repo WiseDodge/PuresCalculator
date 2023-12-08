013dc9f76613bef4db0fe39180161987226b22f2
bwacc <gazastriptease1948@gmail.com>
2023-12-08 23:24:12 +0400

    uwu femoy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,7 @@
-module com.example.purescalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
+module PuresCalculator {
     requires org.json;
-
+    requires javafx.graphics;
+    requires javafx.controls;
 
     opens com.wisedodge.purescalculator to javafx.fxml;
     exports com.wisedodge.purescalculator;
@@ -10,4 +9,5 @@ module com.example.purescalculator {
     opens com.wisedodge.purescalculator.ui to javafx.fxml;
     exports com.wisedodge.purescalculator.converters;
     opens com.wisedodge.purescalculator.converters to javafx.fxml;
+
 }
